755ded290e2cc9f129301c20e48667fe2f2039a1
Jeong Sohyun <84312457+kokiok3@users.noreply.github.com>
2026-02-06 23:50:22 +0900

    Refactor: 랭킹데이터 api 호출하기 및 서버로 이동한 코드/테스트 코드 삭제

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,20 +1,8 @@
-
-const temp = async () => {
-  console.log('temp')
-  const response = await fetch('http://localhost:8000');
-  console.log('response: ', response)
-  const data = await response.text();
-  return data;
-}
 export default async function Home() {
-  const data = await temp()
-  console.log('data: ', data)
-
   return (
     <div>
       <h1 className="text-9xl font-bold  underline">HI</h1><h1 className="text-9xl font-bold  underline">HI</h1><h1 className="text-9xl font-bold  underline">HI</h1><h1 className="text-9xl font-bold  underline">HI</h1><h1 className="text-9xl font-bold  underline">HI</h1><h1 className="text-9xl font-bold  underline">HI</h1><h1 className="text-9xl font-bold  underline">HI</h1><h1 className="text-9xl font-bold  underline">HI</h1><h1 className="text-9xl font-bold  underline">HI</h1><h1 className="text-9xl font-bold  underline">HI</h1><h1 className="text-9xl font-bold  underline">HI</h1><h1 className="text-9xl font-bold  underline">HI</h1><h1 className="text-9xl font-bold  underline">HI</h1><h1 className="text-9xl font-bold  underline">HI</h1><h1 className="text-9xl font-bold  underline">HI</h1><h1 className="text-9xl font-bold  underline">HI</h1><h1 className="text-9xl font-bold  underline">HI</h1>
       <h1 className="text-9xl font-bold  underline">HI</h1>
     </div>
-
   )
 }
