c9847f121b8d9acb03707f7aa9de06eb3dd9d80d
Jeong Sohyun <84312457+kokiok3@users.noreply.github.com>
2026-02-12 00:11:55 +0900

    Feat: RankingTable : 순위 UI 적용, 빈 테이블일 경우 UI 추가

--- apps/web/src/components/RankingTable.tsx
+++ apps/web/src/components/RankingTable.tsx
@@ -1,4 +1,8 @@
 import { Rank, FetchRankQuery, Type } from '@mobinogi/shared';
+import GoldMedal from "assets/images/gold-medal.png"
+import SilverMedal from "assets/images/silver-medal.png"
+import BronzeMedal from "assets/images/bronze-medal.png"
+import Image from 'next/image';
 
 
 const fetchRankingData = async (type: Type) => {
@@ -25,7 +29,7 @@ export default async function RankingTable({ page, type }: { page: number, type:
             <table className="table table-lg bg-white">
                 <thead>
                     <tr>
-                        <th>순위</th>
+                        <th className='text-center'>순위</th>
                         <th>서버</th>
                         <th>캐릭터명</th>
                         <th>클래스</th>
@@ -33,18 +37,59 @@ export default async function RankingTable({ page, type }: { page: number, type:
                     </tr>
                 </thead>
                 <tbody>
-
-                    {pagingList?.map((rank: Rank, i: number) => {
-                        return (
-                            <tr key={rank.name} className="row-hover">
-                                <td className=' py-42'>{i + 1}</td>
-                                <td className=' py-42'>{rank.server}</td>
-                                <td className=' py-42'>{rank.name}</td>
-                                <td className=' py-42'>{rank.class}</td>
-                                <td className=' py-42'>{rank.power}</td>
-                            </tr>
-                        )
-                    })}
+                    {pagingList.length === 0 ?
+                        <tr ><td colSpan={5} className='py-42 text-center'>1,000위 내에 없어요 ㅜㅜ</td></tr> :
+                        pagingList?.map((rank: Rank, i: number) => {
+                            return (
+                                <tr key={rank.name} className="row-hover">
+                                    <td className='py-42 relative text-center'>
+                                        <div className='absolute top-1/2 -translate-y-1/2'>
+                                            {i + 1 + startIndex === 1 &&
+                                                <Image
+                                                    src={GoldMedal}
+                                                    alt='금메달 이미지'
+                                                    width={34}
+                                                    height={34}
+                                                    objectFit='cover'
+                                                    objectPosition='center'
+                                                    unoptimized
+                                                    className='relative '
+                                                    data-source="https://www.flaticon.com/kr/free-animated-icons/"
+                                                ></Image>}
+                                            {i + 1 + startIndex === 2 &&
+                                                <Image
+                                                    src={SilverMedal}
+                                                    alt='은메달 이미지'
+                                                    width={34}
+                                                    height={34}
+                                                    objectFit='cover'
+                                                    objectPosition='center'
+                                                    unoptimized
+                                                    className='relative '
+                                                    data-source="https://www.flaticon.com/kr/free-animated-icons/"
+                                                ></Image>}
+                                            {i + 1 + startIndex === 3 &&
+                                                <Image
+                                                    src={BronzeMedal}
+                                                    alt='동메달 이미지'
+                                                    width={34}
+                                                    height={34}
+                                                    objectFit='cover'
+                                                    objectPosition='center'
+                                                    unoptimized
+                                                    className='relative '
+                                                    data-source="https://www.flaticon.com/kr/free-animated-icons/"
+                                                ></Image>}
+                                        </div>
+                                        <span>{i + 1 + startIndex}</span>
+                                    </td>
+                                    <td className=' py-42'>{rank.server}</td>
+                                    <td className=' py-42'>{rank.name}</td>
+                                    <td className=' py-42'>{rank.class}</td>
+                                    <td className=' py-42'>{rank.power}</td>
+                                </tr>
+                            )
+                        })}
                 </tbody>
             </table>
         </div>
