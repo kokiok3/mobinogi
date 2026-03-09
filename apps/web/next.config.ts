7277bdd94621ac12267868360d50471871970d51
Jeong Sohyun <84312457+kokiok3@users.noreply.github.com>
2026-03-09 22:14:39 +0900

    Update next.config.ts : 경로가 / 일 때 '/rank'로 리다이렉트

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   transpilePackages: ['@mobinogi/shared'], // 👈 외부 패키지를 트랜스파일하도록 명시
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/rank',
+        permanent: true,
+      }
+    ];
+  },
 };
 
 export default nextConfig;
