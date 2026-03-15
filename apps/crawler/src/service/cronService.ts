b83760035c65264d2f590e86f422ac67ec202b81
Jeong Sohyun <84312457+kokiok3@users.noreply.github.com>
2026-03-15 18:38:23 +0900

    Test: cronService.ts : 전투력부분만 크롤링

--- apps/crawler/src/service/cronService.ts
+++ apps/crawler/src/service/cronService.ts
@@ -6,7 +6,7 @@ import { setRankData } from '#src/models/rankModel';
 // cron 
 // 초(선택) 분 시 일 달 주
 export const cronRanking = () => {
-    cron.schedule('0 * * * *', async () => {
+    cron.schedule('45 * * * *', async () => {
         try {
 
             const powerList: Rank[] = await getAllServerRank(TYPE.power);
@@ -16,24 +16,36 @@ export const cronRanking = () => {
             console.log('crontab Error:', error)
         }
     }, { timezone: 'Asia/Seoul' });
-
-    cron.schedule('0 * * * *', async () => {
-        try {
-            const charmList: Rank[] = await getAllServerRank(TYPE.charm);
-            setRankData(TYPE.charm, charmList);
-
-        } catch (error) {
-            console.log('crontab Error:', error)
-        }
-    }, { timezone: 'Asia/Seoul' });
-
-    cron.schedule('0 * * * *', async () => {
-        try {
-            const livingList: Rank[] = await getAllServerRank(TYPE.living);
-            setRankData(TYPE.living, livingList);
-
-        } catch (error) {
-            console.log('crontab Error:', error)
-        }
-    }, { timezone: 'Asia/Seoul' });
 }
+// export const cronRanking = () => {
+//     cron.schedule('0 * * * *', async () => {
+//         try {
+
+//             const powerList: Rank[] = await getAllServerRank(TYPE.power);
+//             setRankData(TYPE.power, powerList);
+
+//         } catch (error) {
+//             console.log('crontab Error:', error)
+//         }
+//     }, { timezone: 'Asia/Seoul' });
+
+//     cron.schedule('0 * * * *', async () => {
+//         try {
+//             const charmList: Rank[] = await getAllServerRank(TYPE.charm);
+//             setRankData(TYPE.charm, charmList);
+
+//         } catch (error) {
+//             console.log('crontab Error:', error)
+//         }
+//     }, { timezone: 'Asia/Seoul' });
+
+//     cron.schedule('0 * * * *', async () => {
+//         try {
+//             const livingList: Rank[] = await getAllServerRank(TYPE.living);
+//             setRankData(TYPE.living, livingList);
+
+//         } catch (error) {
+//             console.log('crontab Error:', error)
+//         }
+//     }, { timezone: 'Asia/Seoul' });
+// }
