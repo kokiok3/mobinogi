dceef5f6ba442d042580c7674658447d3ce1623d
Jeong Sohyun <84312457+kokiok3@users.noreply.github.com>
2026-03-17 23:35:47 +0900

    Design: rank페이지에 반응형 추가

--- apps/web/src/app/rank/page.tsx
+++ apps/web/src/app/rank/page.tsx
@@ -19,84 +19,88 @@ export default async function PageRank({ searchParams }: { searchParams: Promise
 	const typeKorean: TypeKorean = TYPE_KOREAN[currentType]
 
 	return (
-		<div className="mt-60 px-200">
-			{/* 배경 */}
-			<div className="animate-floating-left fixed left-0 bottom-100 w-300 h-300 bra bg-linear-to-r from-yellow-500 to-orange-200 blur-[50px] rounded-full opacity-[0.3]"></div>
-			<div className="animate-floating-right fixed -right-150 top-200 w-700 h-500 bra bg-linear-to-r from-orange-500 to-orange-50 blur-[70px] rounded-full opacity-[0.3]"></div>
+		<div className="mt-60 xl:px-200">
+			<div className="">
+				{/* 배경 */}
+				<div className="animate-floating-left fixed left-0 bottom-100 -z-1 w-300 h-300 bra bg-linear-to-r from-yellow-500 to-orange-200 blur-[50px] rounded-full opacity-[0.3]"></div>
+				<div className="animate-floating-right fixed -right-150 top-200 -z-1 w-700 h-500 bra bg-linear-to-r from-orange-500 to-orange-50 blur-[70px] rounded-full opacity-[0.3]"></div>
 
-			{/* 타이틀 */}
-			<div className=''>
-				<div className='relative flex flex-col items-center gap-14 w-fit mx-auto'>
-					{currentType === TYPE.power &&
-						<Image
-							src={Sword}
-							alt='검 애니메이션 아이콘'
-							width={56}
-							height={56}
-							objectFit='cover'
-							objectPosition='center'
-							unoptimized
-							className='absolute left-6 -top-12'
-							data-source="https://www.flaticon.com/kr/free-animated-icons/"
-						></Image>
+				{/* 타이틀 */}
+				<div className=''>
+					<div className='relative flex flex-col items-center gap-14 w-fit mx-auto'>
+						{currentType === TYPE.power &&
+							<Image
+								src={Sword}
+								alt='검 애니메이션 아이콘'
+								width={56}
+								height={56}
+								objectFit='cover'
+								objectPosition='center'
+								unoptimized
+								className='absolute left-6 -top-12'
+								data-source="https://www.flaticon.com/kr/free-animated-icons/"
+							></Image>
 
-					}
-					{currentType === TYPE.charm &&
-						<Image
-							src={Crown}
-							alt='왕관 애니메이션 아이콘'
-							width={56}
-							height={56}
-							objectFit='cover'
-							objectPosition='center'
-							unoptimized
-							className='absolute left-16 -top-12'
-							data-source="https://www.flaticon.com/kr/free-animated-icons/"
-						></Image>}
-					{currentType === TYPE.living &&
-						<Image
-							src={Fish}
-							alt='물고기 애니메이션 아이콘'
-							width={54}
-							height={54}
-							objectFit='cover'
-							objectPosition='center'
-							unoptimized
-							className='absolute left-22 -top-10'
-							data-source="https://www.flaticon.com/kr/free-animated-icons/"
-						></Image>}
-					{currentType === TYPE.mix &&
-						<Image
-							src={Trophy}
-							alt='트로피 애니메이션 아이콘'
-							width={50}
-							height={50}
-							objectFit='cover'
-							objectPosition='center'
-							unoptimized
-							className='absolute left-19 -top-6'
-							data-source="https://www.flaticon.com/kr/free-animated-icons/"
-						></Image>}
-					<span className='ml-40 text-[36px] font-bold'>{typeKorean} 랭킹</span>
-					<span className='text-[16px] font-medium text-gray-500'>1위부터 1,000위까지 랭킹을 확인하세요.</span>
+						}
+						{currentType === TYPE.charm &&
+							<Image
+								src={Crown}
+								alt='왕관 애니메이션 아이콘'
+								width={56}
+								height={56}
+								objectFit='cover'
+								objectPosition='center'
+								unoptimized
+								className='absolute left-16 -top-12'
+								data-source="https://www.flaticon.com/kr/free-animated-icons/"
+							></Image>}
+						{currentType === TYPE.living &&
+							<Image
+								src={Fish}
+								alt='물고기 애니메이션 아이콘'
+								width={54}
+								height={54}
+								objectFit='cover'
+								objectPosition='center'
+								unoptimized
+								className='absolute left-22 -top-10'
+								data-source="https://www.flaticon.com/kr/free-animated-icons/"
+							></Image>}
+						{currentType === TYPE.mix &&
+							<Image
+								src={Trophy}
+								alt='트로피 애니메이션 아이콘'
+								width={50}
+								height={50}
+								objectFit='cover'
+								objectPosition='center'
+								unoptimized
+								className='absolute left-19 -top-6'
+								data-source="https://www.flaticon.com/kr/free-animated-icons/"
+							></Image>}
+						<span className='ml-40 text-[36px] font-bold'>{typeKorean} 랭킹</span>
+						<span className='text-[16px] font-medium text-gray-500'>1위부터 500위까지 랭킹을 확인하세요.</span>
+					</div>
 				</div>
+
+				{/* 테이블 헤더 */}
+				<div className='rank__header'>
+					<span className='text-gray-500 text-[12px]'>
+						매 00분에 랭킹 정보가 업데이트 됩니다. 랭킹에 반영되기까지 일정 시간이 소요될 수 있습니다.
+					</span>
+
+					<Search />
+				</div>
+				{/* 테이블 헤더 끝 */}
+
+				{/* 테이블 */}
+				<RankingTable page={currentPage} type={currentType}></RankingTable>
+				{/* 테이블 끝 */}
+
+				{/* 페이지네이션 */}
+				<Pagination type={currentType}></Pagination>
+				{/* 페이지네이션 끝 */}
 			</div>
-
-			{/* 테이블 헤더 */}
-			<div className='flex items-center justify-between mt-60'>
-				<span className='text-gray-500 text-[12px]'>게임 내 수치가 변경된 경우, 캐릭터 정보가 랭킹에 반영되기까지 일정 시간이 소요될 수 있습니다.</span>
-
-				<Search />
-			</div>
-			{/* 테이블 헤더 끝 */}
-
-			{/* 테이블 */}
-			<RankingTable page={currentPage} type={currentType}></RankingTable>
-			{/* 테이블 끝 */}
-
-			{/* 페이지네이션 */}
-			<Pagination type={currentType}></Pagination>
-			{/* 페이지네이션 끝 */}
 		</div>
 
 	)
--- apps/web/src/components/RankingTable.tsx
+++ apps/web/src/components/RankingTable.tsx
@@ -1,4 +1,4 @@
-import { Rank, FetchRankQuery, Type } from '@mobinogi/shared';
+import { Rank, FetchRankQuery, Type, TYPE_KOREAN } from '@mobinogi/shared';
 import GoldMedal from "@/assets/images/gold-medal.png"
 import SilverMedal from "@/assets/images/silver-medal.png"
 import BronzeMedal from "@/assets/images/bronze-medal.png"
@@ -25,77 +25,153 @@ export default async function RankingTable({ page, type }: { page: number, type:
     const startIndex = (page - 1) * 20
     const pagingList = rankingList.splice(startIndex, 20);
 
+    const isShowRank = (index: number) => {
+        return (index + 1 + startIndex === 1 ||
+            index + 1 + startIndex === 2 ||
+            index + 1 + startIndex === 3 ? 'hidden' : '')
+    }
+
     return (
-        <div className="mt-16 card border-base-content/10 border">
-            <table className="table table-lg bg-white">
-                <thead>
-                    <tr>
-                        <th className='text-center'>순위</th>
-                        <th>서버</th>
-                        <th>캐릭터명</th>
-                        <th>클래스</th>
-                        <th>전투력</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {pagingList.length === 0 ?
-                        <tr ><td colSpan={5} className='py-42 text-center'>1,000위 내에 없어요 ㅜㅜ</td></tr> :
-                        pagingList?.map((rank: Rank, i: number) => {
-                            return (
-                                <tr key={rank.name} className="row-hover">
-                                    <td className='py-42 relative text-center'>
-                                        <div className='absolute top-1/2 -translate-y-1/2'>
-                                            {i + 1 + startIndex === 1 &&
-                                                <Image
-                                                    src={GoldMedal}
-                                                    alt='금메달 이미지'
-                                                    width={34}
-                                                    height={34}
-                                                    objectFit='cover'
-                                                    objectPosition='center'
-                                                    unoptimized
-                                                    className='relative '
-                                                    data-source="https://www.flaticon.com/kr/free-animated-icons/"
-                                                ></Image>}
-                                            {i + 1 + startIndex === 2 &&
-                                                <Image
-                                                    src={SilverMedal}
-                                                    alt='은메달 이미지'
-                                                    width={34}
-                                                    height={34}
-                                                    objectFit='cover'
-                                                    objectPosition='center'
-                                                    unoptimized
-                                                    className='relative '
-                                                    data-source="https://www.flaticon.com/kr/free-animated-icons/"
-                                                ></Image>}
-                                            {i + 1 + startIndex === 3 &&
-                                                <Image
-                                                    src={BronzeMedal}
-                                                    alt='동메달 이미지'
-                                                    width={34}
-                                                    height={34}
-                                                    objectFit='cover'
-                                                    objectPosition='center'
-                                                    unoptimized
-                                                    className='relative '
-                                                    data-source="https://www.flaticon.com/kr/free-animated-icons/"
-                                                ></Image>}
-                                        </div>
-                                        <span>{i + 1 + startIndex}</span>
-                                    </td>
-                                    <td className=' py-42'>{rank.server}</td>
-                                    <td className=' py-42'>{rank.name}</td>
-                                    <td className=' py-42'>{rank.class}</td>
-                                    <td className=' py-42'>{rank.power}</td>
-                                </tr>
-                            )
-                        })}
-                </tbody>
-            </table>
+        <div>
+            <div className="hidden xl:block mt-16 card border-base-content/10 border">
+                <table className="table table-lg bg-white ">
+                    <thead>
+                        <tr>
+                            <th className='text-center'>순위</th>
+                            <th>서버</th>
+                            <th>캐릭터명</th>
+                            <th>클래스</th>
+                            <th>{TYPE_KOREAN[type]}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {pagingList.length === 0 ?
+                            <tr ><td colSpan={5} className='py-42 text-center'>500위 내에 없어요 ㅜㅜ</td></tr>
+                            : pagingList?.map((rank: Rank, i: number) => {
+                                return (
+                                    <tr key={rank.name} className="row-hover">
+                                        <td className='py-42 relative text-center'>
+                                            <div className='absolute top-1/2 left-1/2 -translate-1/2'>
+                                                {i + 1 + startIndex === 1 &&
+                                                    <Image
+                                                        src={GoldMedal}
+                                                        alt='금메달 이미지'
+                                                        width={34}
+                                                        height={34}
+                                                        objectFit='cover'
+                                                        objectPosition='center'
+                                                        unoptimized
+                                                        className='relative '
+                                                        data-source="https://www.flaticon.com/kr/free-animated-icons/"
+                                                    ></Image>}
+                                                {i + 1 + startIndex === 2 &&
+                                                    <Image
+                                                        src={SilverMedal}
+                                                        alt='은메달 이미지'
+                                                        width={34}
+                                                        height={34}
+                                                        objectFit='cover'
+                                                        objectPosition='center'
+                                                        unoptimized
+                                                        className='relative '
+                                                        data-source="https://www.flaticon.com/kr/free-animated-icons/"
+                                                    ></Image>}
+                                                {i + 1 + startIndex === 3 &&
+                                                    <Image
+                                                        src={BronzeMedal}
+                                                        alt='동메달 이미지'
+                                                        width={34}
+                                                        height={34}
+                                                        objectFit='cover'
+                                                        objectPosition='center'
+                                                        unoptimized
+                                                        className='relative '
+                                                        data-source="https://www.flaticon.com/kr/free-animated-icons/"
+                                                    ></Image>}
+                                            </div>
+                                            <span className={isShowRank(i)}>{i + 1 + startIndex}</span>
+                                        </td>
+                                        <td className='py-42'>{rank.server}</td>
+                                        <td className='py-42'>{rank.name}</td>
+                                        <td className='py-42'>{rank.class}</td>
+                                        <td className='py-42'>{Number(rank.power).toLocaleString()}</td>
+                                    </tr>
+                                )
+                            })}
+                    </tbody>
+                </table>
+            </div>
+
+            <div className='rank__list xl:hidden mt-16 px-30 bg-white'>
+                {pagingList.length === 0 ? <span className='py-50 text-center text-gray-500'>500위 내에 없어요 ㅜㅜ</span>
+                    : pagingList?.map((rank: Rank, i: number) => {
+                        return (
+                            <div key={rank.name} className='rank__item'>
+                                <div>
+                                    <div>{i + 1 + startIndex === 1 &&
+                                        <Image
+                                            src={GoldMedal}
+                                            alt='금메달 이미지'
+                                            width={34}
+                                            height={34}
+                                            objectFit='cover'
+                                            objectPosition='center'
+                                            unoptimized
+                                            className='relative '
+                                            data-source="https://www.flaticon.com/kr/free-animated-icons/"
+                                        ></Image>}
+                                        {i + 1 + startIndex === 2 &&
+                                            <Image
+                                                src={SilverMedal}
+                                                alt='은메달 이미지'
+                                                width={34}
+                                                height={34}
+                                                objectFit='cover'
+                                                objectPosition='center'
+                                                unoptimized
+                                                className='relative '
+                                                data-source="https://www.flaticon.com/kr/free-animated-icons/"
+                                            ></Image>}
+                                        {i + 1 + startIndex === 3 &&
+                                            <Image
+                                                src={BronzeMedal}
+                                                alt='동메달 이미지'
+                                                width={34}
+                                                height={34}
+                                                objectFit='cover'
+                                                objectPosition='center'
+                                                unoptimized
+                                                className='relative '
+                                                data-source="https://www.flaticon.com/kr/free-animated-icons/"
+                                            ></Image>}
+                                        <span className={isShowRank(i)}>{i + 1 + startIndex}</span>
+                                    </div>
+                                </div>
+                                <div className='rank__info'>
+                                    <span className='w-120'>서버</span>
+                                    <span>{rank.server}</span>
+                                </div>
+                                <div className='rank__info'>
+                                    <span className='w-120'>캐릭터명</span>
+                                    <span>{rank.name}</span>
+                                </div>
+                                <div className='rank__info'>
+                                    <span className='w-120'>클래스</span>
+                                    <span>{rank.class}</span>
+                                </div>
+                                <div className='rank__info'>
+                                    <span className='w-120'>{TYPE_KOREAN
+                                    [type]}</span>
+                                    <span>{Number(rank.power).toLocaleString()}</span>
+                                </div>
+                            </div>
+                        )
+                    })
+
+                }
+
+            </div>
         </div>
-
-
 
     )
 }
