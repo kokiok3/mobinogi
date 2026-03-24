ef206ced328e85cb1b517186b657f016ef72fdcf
정소현 <84312457+kokiok3@users.noreply.github.com>
2026-03-24 17:12:52 +0900

    Update layout.tsx metadata

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -6,8 +6,49 @@ import "@/styles/global.css"
 import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
-  title: "모비랭크",
-  description: "모비랭크에서는 서버종합 랭킹을 한눈에 볼 수 있어요.",
+  title: '모비랭크 | 마비노기 모바일 서버 통합 랭킹',
+  description: '마비노기 모바일 서버 통합 랭킹 TOP500을 확인해 보세요.',
+  applicationName: '모비랭크',
+  keywords: ['모비노기', '모비랭크', '모비랭킹', '모비랭커', '마비노기', '마비노기 모바일', '랭킹', '랭크', '순위', '전투력', '매력', '생활', '통합', '서버통합'],
+  authors: [{name: '모비랭크'}, {name: 'kkokkiok3'}],
+  creators: '모비랭크',
+  publisher: '모비랭크',
+  openGraph: {
+    title: '모비랭크 | 마비노기 모바일 서버 통합 랭킹',
+    description: '마비노기 모바일 서버 통합 랭킹 TOP500을 확인해 보세요.',
+    url: 'https://mobinogi-rank.vercel.app',
+    siteName: '모비랭크',
+    authors: ['모비랭크'],
+    type: 'website',
+    locale: 'ko_KR'
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: '모비랭크',
+      description: '마비노기 모바일 서버 통합 랭킹 TOP500을 확인해 보세요.',
+      creator: '모비랭크',
+    },
+    // <meta name="google-site-verification content="값" />으로 변환된다.
+    // verification: {
+    //   google: '값',
+    // },
+    // <meta name="naver-site-verification" content="값" />으로 변환된다.
+    // other: {
+    //   'naver-site-verification': '값',
+    // },
+  },
 };
 
 export default function RootLayout({
